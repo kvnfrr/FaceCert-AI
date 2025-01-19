f59bf1fbadb52dfbb1279a8fa9fd15727e0e0894
kvnfrr <kevinferrercs@gmail.com>
2025-01-18 20:39:55 -0500

    Initial commit

---
+++ src/main/java/com/facecert/facecertai/FaceCertAiApplication.java
@@ -0,0 +1,13 @@
+package com.facecert.facecertai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceCertAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceCertAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facecert/facecertai/FaceCertAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facecert.facecertai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceCertAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
